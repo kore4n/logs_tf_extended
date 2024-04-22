8332b4e790d4dc919d1495106e48d411f737e39d
Jason Kwan <kwan.jason2001@gmail.com>
2024-04-22 18:15:40 -0400

    v1.1
    save player data to local storage cache for reuse

--- display_extended_column.js
+++ display_extended_column.js
@@ -8,12 +8,15 @@ const RGLDivisions = Object.freeze({
   Invite: 6,
 });
 
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
 // gamemode = "Sixes" or "Highlander"
 async function GetHighestGamemodeTeam(gamemode, steamID) {
   const res = await GetRGLPastTeams(steamID);
 
   if (res.status != 200) {
     console.log(res);
+    return;
   }
 
   const pastTeams = await res.json();
@@ -46,30 +49,44 @@ async function GetRGLPastTeams(steamID) {
 
   const response = await fetch(uri);
 
+  await timer(300);
+
   return response;
 }
 
-async function GetRGLName(steamID) {
+async function GetRGLProfile(steamID) {
   const uri = `https://api.rgl.gg/v0/profile/${steamID}`;
 
   const response = await fetch(uri);
 
   // 325ms and below are too fast - it will exceed rate limit.
   // await timer(325);
-  // await timer(200);
+  await timer(300);
 
-  return response;
+  if (response.status != 200) {
+    console.log(response);
+    return;
+  }
+
+  const data = await response.json();
+
+  return data;
 }
 
 async function GetETF2LName(steamID) {
   const uriETF2L = `https://api-v2.etf2l.org/player/${steamID}`;
   const responseETF2L = await fetch(uriETF2L);
 
-  await timer(200);
-  return responseETF2L;
-}
+  await timer(300);
 
-const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+  if (responseETF2L.status != 200) {
+    console.log(responseETF2L);
+    return;
+  }
+
+  const data = await responseETF2L.json();
+  return data;
+}
 
 async function UpdateETF2L() {
   for (let i = 0; i < playerRows.length; i++) {
@@ -97,41 +114,51 @@ async function UpdateETF2L() {
   }
 }
 
-async function updateRGLName(steamID, rglElement) {
-  const res = await GetRGLName(steamID);
+async function UpdateETF2LName(steamID, playerInfo, leagueElement) {
+  // Get rid of 'Loading...' message
+  leagueElement.innerHTML = "";
 
-  if (res.status != 200) {
-    console.log(res);
-    return;
-  }
+  if (!playerInfo.etf2l.name) return;
 
-  const data = await res.json();
+  // const data = await resETF2L.json();
+  const etf2lLink = document.createElement("a");
+  etf2lLink.innerHTML = playerInfo.etf2l.name;
+  etf2lLink.href = `https://etf2l.org/search/${steamID}/`;
+  etf2lLink.target = "_blank";
+  etf2lLink.style.backgroundColor = "rgb(144, 238, 144)";
+  etf2lLink.style.padding = "6px";
+  leagueElement.appendChild(etf2lLink);
+}
+
+async function UpdateRGLName(steamID, playerInfo, leagueElement) {
+  if (!playerInfo.rgl.name) return;
+
   const rglLink = document.createElement("a");
   rglLink.href = `https://rgl.gg/Public/PlayerProfile?p=${steamID}`;
   rglLink.target = "_blank";
   rglLink.style.backgroundColor = "rgb(255, 203, 108)";
   rglLink.style.padding = "6px";
   rglLink.style.marginLeft = "10px";
-  rglLink.innerHTML = data.name;
 
-  rglElement.appendChild(rglLink);
+  const banWarning = playerInfo.rgl.isBanned;
 
-  const banWarning = data.status.isBanned;
+  rglLink.innerHTML = playerInfo.rgl.name;
 
-  rglLink.innerHTML = data.name;
+  leagueElement.appendChild(rglLink);
 
   if (!banWarning) return;
 
   const banWarningSpan = document.createElement("span");
-  banWarningSpan.innerHTML = "(BANNED)";
+  banWarningSpan.innerHTML = " (BANNED)";
   rglLink.style.backgroundColor = "rgb(255, 31, 31)";
 
   rglLink.appendChild(banWarningSpan);
 }
 
-async function updateRGLDivision(steamID, rglElement) {
-  const highestSixesTeam = await GetHighestGamemodeTeam("Sixes", steamID);
+async function UpdateRGLDivision(playerInfo, leagueElement) {
+  if (!playerInfo.rgl.name) return;
 
+  const highestSixesTeam = playerInfo.rgl.division;
   let sixesDivisionString = "";
   if (highestSixesTeam != "None") {
     sixesDivisionString = `${highestSixesTeam}`;
@@ -144,25 +171,71 @@ async function updateRGLDivision(steamID, rglElement) {
     rglDivisionElement.style.padding = "6px";
     rglDivisionElement.style.marginLeft = "10px";
 
-    rglElement.appendChild(rglDivisionElement);
+    leagueElement.appendChild(rglDivisionElement);
   }
 }
 
-async function UpdateRGL() {
+async function FetchPlayerInfo(steamID) {
+  const RGL_profile_data = await GetRGLProfile(steamID);
+
+  const etf2l_name = await GetETF2LName(steamID);
+
+  const highest_rgl_division = await GetHighestGamemodeTeam("Sixes", steamID);
+
+  const playerInfoToInsert = {
+    rgl: {
+      name: RGL_profile_data ? RGL_profile_data.name : null,
+      isBanned: RGL_profile_data ? RGL_profile_data.status.isBanned : null,
+      division: highest_rgl_division ? highest_rgl_division : null,
+    },
+    etf2l: {
+      name: etf2l_name ? etf2l_name.player.name : null,
+    },
+  };
+
+  return playerInfoToInsert;
+}
+
+async function UpdatePlayerRows() {
+  const listOfSteamIDsInStorageThatMightNeedUpdating = [];
+
   for (let i = 0; i < playerRows.length; i++) {
     const steamID = playerRows[i].id.split("_")[1];
 
-    const rglElement = playerRows[i].firstChild;
+    const leagueElement = playerRows[i].firstChild;
 
-    await updateRGLName(steamID, rglElement);
-    await updateRGLDivision(steamID, rglElement);
+    const playerInfoStorage = window.localStorage.getItem(steamID);
+    let playerInfo;
+    if (playerInfoStorage) {
+      playerInfo = JSON.parse(playerInfoStorage);
+      listOfSteamIDsInStorageThatMightNeedUpdating.push(steamID);
+    } else {
+      const playerInfoToInsert = await FetchPlayerInfo(steamID);
+
+      window.localStorage.setItem(steamID, JSON.stringify(playerInfoToInsert));
+      playerInfo = playerInfoToInsert;
+    }
+
+    UpdateETF2LName(steamID, playerInfo, leagueElement);
+    UpdateRGLName(steamID, playerInfo, leagueElement);
+    UpdateRGLDivision(playerInfo, leagueElement);
   }
-}
 
-function UpdatePlayerRows() {
-  // Do ETF2L first then do RGL
-  UpdateETF2L();
-  UpdateRGL();
+  // Update all of them in local storage
+  for (let i = 0; i < listOfSteamIDsInStorageThatMightNeedUpdating.length; i++) {
+    const steamID = listOfSteamIDsInStorageThatMightNeedUpdating[i];
+    const playerInfoToInsert = await FetchPlayerInfo(steamID);
+
+    const a = window.localStorage.getItem(steamID);
+
+    if (JSON.stringify(playerInfoToInsert) != a) {
+      // console.log("mismatch. reloading.");
+      window.location.reload(true);
+    }
+
+    window.localStorage.setItem(steamID, JSON.stringify(playerInfoToInsert));
+    playerInfo = playerInfoToInsert;
+  }
 }
 
 const tableBody = document.getElementById("players");
